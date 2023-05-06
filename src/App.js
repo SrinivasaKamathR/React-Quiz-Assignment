2cfe0c9276f5e7a640523cf8d6153b1ce7930761
SrinivasaKamathR <kamathsri1997@gmail.com>
2023-05-06 08:19:52 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoadingSpinner from "./UI/LoadingSpinner";
+const Home = React.lazy(() => import("./Components/Home"));
+const Instructions = React.lazy(() => import("./Components/Instructions"));
+const Quiz = React.lazy(() => import("./Components/Quiz"));
 
+//import Home from "./Components/Home";
+//import Instructions from "./Components/Instructions";
+//import Quiz from "./Components/Quiz";
 function App() {
+  const [startTimer, setStartTimer] = useState(false);
+  const [name, setName] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={<LoadingSpinner />}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home setName={setName} name={name} />} />
+          <Route
+            path="/quiz"
+            element={<Quiz startTimer={startTimer} setName={setName} />}
+          />
+          <Route
+            path="/instructions"
+            element={<Instructions setStartTimer={setStartTimer} name={name} />}
+          />
+        </Routes>
+      </Router>
+    </Suspense>
   );
 }
 
---
+++ src/QuizData/quizdata.js
@@ -0,0 +1,133 @@
+export const quizQuestions = [
+  {
+    id: 1,
+    question: "What is the correct command to create a new React project?",
+    answers: [
+      {
+        id: 1,
+        ans: "npx create-react-app",
+        isCorrect: false,
+      },
+      {
+        id: 2,
+        ans: "npx create-react-app appName",
+        isCorrect: true,
+      },
+      {
+        id: 3,
+        ans: "npm create-react-app",
+        isCorrect: false,
+      },
+      {
+        id: 4,
+        ans: "npx create react-app",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    id: 2,
+    question: "What is the default port number in which the application run ?",
+    answers: [
+      {
+        id: 1,
+        ans: "3000",
+        isCorrect: true,
+      },
+      {
+        id: 2,
+        ans: "8080",
+        isCorrect: false,
+      },
+      {
+        id: 3,
+        ans: "4200",
+        isCorrect: false,
+      },
+      {
+        id: 4,
+        ans: "4000",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    id: 3,
+    question: "Which of the following is true regarding Babel ?",
+    answers: [
+      {
+        id: 1,
+        ans: "Compiler",
+        isCorrect: false,
+      },
+      {
+        id: 2,
+        ans: "Transpiler",
+        isCorrect: false,
+      },
+      {
+        id: 3,
+        ans: "Both a and b",
+        isCorrect: true,
+      },
+      {
+        id: 4,
+        ans: "None of the above",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    id: 4,
+    question: "Which of the following is true about key props ?",
+    answers: [
+      {
+        id: 1,
+        ans: "Key props are used to look beauty",
+        isCorrect: false,
+      },
+      {
+        id: 2,
+        ans: "Key prop is a way to identify the newly added element",
+        isCorrect: true,
+      },
+      {
+        id: 3,
+        ans: "It is one type of attribute of HTML",
+        isCorrect: false,
+      },
+      {
+        id: 4,
+        ans: "It is not used in the array",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    id: 5,
+    question: "Reactjs is written in",
+    answers: [
+      {
+        id: 1,
+        ans: "Swift",
+        isCorrect: false,
+      },
+      {
+        id: 2,
+        ans: "Javascript",
+        isCorrect: true,
+      },
+      {
+        id: 3,
+        ans: "AJAX",
+        isCorrect: false,
+      },
+      {
+        id: 4,
+        ans: "None",
+        isCorrect: false,
+      },
+    ],
+  },
+ 
+];
